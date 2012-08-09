6e9b7a3a1f37073cabe77a1805d411269599ccbc
Vic <vic@kiwi.la>
2012-08-09 20:43:59 +0200

    Fixed Dutch spelling mistake

--- script/lang/themes/oreo/both.php
+++ script/lang/themes/oreo/both.php
@@ -47,7 +47,7 @@
 <div style="border: none; border-bottom: solid 1px rgba(0,0,0,0.2); margin-bottom:27px;"></div>
 <h2 style="margin-top: -10px" id="installation">Deze site gebruikt cookies</h2>
 
-<p>Om deze website goed te laten werken onze website te kunnen optimaliseren op uw beleving plaatsen wij kleine bestanden op uw computer (zogenaamde cookies) en analyseren wij het websitegebruik. Een zeer groot deel van de websites doen dit. Nieuwe wetgeving verplicht ons u hiervoor toestemming te vragen. U kunt <a href="#privacy_nl" onClick="show_nl()">hier</a> lezen hoe wij met uw privacy omgaan en <a href="#cookies_nl" onClick="show_nl()">hier</a> kunt u meer informatie vinden over cookies.</p>
+<p>Om deze website goed te laten werken en onze website te kunnen optimaliseren op uw beleving plaatsen wij kleine bestanden op uw computer (zogenaamde cookies) en analyseren wij het websitegebruik. Een zeer groot deel van de websites doen dit. Nieuwe wetgeving verplicht ons u hiervoor toestemming te vragen. U kunt <a href="#privacy_nl" onClick="show_nl()">hier</a> lezen hoe wij met uw privacy omgaan en <a href="#cookies_nl" onClick="show_nl()">hier</a> kunt u meer informatie vinden over cookies.</p>
 <div style="background: url('http://kiwiboom.github.com/kiwicookie/images/hr.gif');border: 0;height: 11px;margin-bottom:20px;"></div>
 
 <div style="margin: 0 auto; margin-top: -15px; margin-bottom: 5px; width: 350px;" id="links"><a style="float: left;" href="<?php echo $current."/cookie_policy?a"; ?>">Ik ga akkoord</a><a style="float: right;" href="<?php echo $current."/cookie_policy?d"; ?>">Ik ga niet akkoord</a><br/></div>
