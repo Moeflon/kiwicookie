feb28a53362094985a45d153c2d9a6e452d5566f
Vic <vic@kiwi.la>
2012-08-10 15:15:51 +0200

    Added mobile detection and removed iPad from mobile.

--- script/cookie.php
+++ script/cookie.php
@@ -94,6 +94,15 @@ else {
 	}
 }
 
+// Detect iPhone for theme
+function isMobile() {   
+    if(preg_match('/(alcatel|amoi|android|avantgo|blackberry|benq|cell|cricket|docomo|elaine|htc|iemobile|iphone|ipaq|ipod|j2me|java|midp|mini|mmp|mobi|motorola|nec-|nokia|palm|panasonic|philips|phone|sagem|sharp|sie-|smartphone|sony|symbian|t-mobile|telus|up\.browser|up\.link|vodafone|wap|webos|wireless|xda|xoom|zte)/i', $_SERVER['HTTP_USER_AGENT']) && !preg_match('/(ipad)/i', $_SERVER['HTTP_USER_AGENT'])) {
+    return true;
+}
+else
+    return false;
+}
+
 // Make it work on root
 if($_SERVER['REQUEST_URI'] == "/") {
 	$current = "";
